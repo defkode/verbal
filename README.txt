9a7d680de6503cfc3ee6f5e1d5d34d25d7e7ac0b
Tomasz Mazur <mazzy@trix.pl>
2008-07-31 16:37:50 +0200

    support for American English numbering system, British to come...

--- README.txt
+++ README.txt
@@ -2,14 +2,22 @@
 
 script/plugin install git://github.com/tomaszmazur/verbal.git
 
-
 == USAGE:
 
-Verbal::Numbers.to_words(123_456_789)
+NUMBERS:
+
+Verbal::Polish::Numbers.to_words(123_456_789)
 => "sto dwadzieścia trzy miliony czterysta pięćdziesiąt sześć tysiecy siedemset osiemdziesiąt dziewięć"
 
-# NOTE: accepts only integers, value must be provided in 'grosze' 1zł = 100gr
-Verbal::Currency.to_words(123_456_789_00, 'PLN')
+NOTE: It's American English numbering system 
+http://www.mazes.com/AmericanNumberingSystem.html
+
+Verbal::English::Numbers.to_words(123_456_789)
+=> "one hundred twenty-three million four hundred fifty-six thousand seven hundred and eighty-nine"
+
+CURRENCIES:
+
+Verbal::Polish::Currency.to_words(123_456_789_00, 'PLN')
 => "sto dwadzieścia trzy miliony czterysta pięćdziesiąt sześć tysiecy siedemset osiemdziesiąt dziewięć złotych zero groszy"
 
-
+No Support for Verbal::English::Currency yet... Anybody?
--- spec/unit/numbers.rb
+++ spec/unit/numbers.rb
@@ -3,29 +3,29 @@ require File.join(File.dirname(__FILE__), '..', 'spec_helper')
 describe Verbal::Numbers do
 
   it 'should return correct string' do
-    Verbal::Numbers.to_words(0).should      == 'zero'
-    Verbal::Numbers.to_words(101).should    == 'sto jeden'
-    Verbal::Numbers.to_words(15).should     == 'piętnaście'
-    Verbal::Numbers.to_words(21).should     == 'dwadzieścia jeden'
-    Verbal::Numbers.to_words(1_592).should  == 'tysiąc pięćset dziewięćdziesiąt dwa'
-    Verbal::Numbers.to_words(1_000).should  == 'tysiąc'
+    Verbal::Polish::Numbers.to_words(0).should      == 'zero'
+    Verbal::Polish::Numbers.to_words(101).should    == 'sto jeden'
+    Verbal::Polish::Numbers.to_words(15).should     == 'piętnaście'
+    Verbal::Polish::Numbers.to_words(21).should     == 'dwadzieścia jeden'
+    Verbal::Polish::Numbers.to_words(1_592).should  == 'tysiąc pięćset dziewięćdziesiąt dwa'
+    Verbal::Polish::Numbers.to_words(1_000).should  == 'tysiąc'
   end
 
   it 'should variate strings depending on grammatical gender' do
-    Verbal::Numbers.to_words(1, 0).should     == 'jeden'
-    Verbal::Numbers.to_words(1, 1).should     == 'jedna'
-    Verbal::Numbers.to_words(1, 2).should     == 'jedno'
-    Verbal::Numbers.to_words(2, 0).should     == 'dwa'
-    Verbal::Numbers.to_words(2, 1).should     == 'dwie'
-    Verbal::Numbers.to_words(2, 2).should     == 'dwa'
-    Verbal::Numbers.to_words(101, 0).should   == 'sto jeden'
-    Verbal::Numbers.to_words(101, 1).should   == 'sto jeden'
-    Verbal::Numbers.to_words(102, 0).should   == 'sto dwa'
-    Verbal::Numbers.to_words(102, 1).should   == 'sto dwie'
-    Verbal::Numbers.to_words(1_001, 0).should == 'tysiąc jeden'
-    Verbal::Numbers.to_words(1_001, 1).should == 'tysiąc jeden'
-    Verbal::Numbers.to_words(1_002, 0).should == 'tysiąc dwa'
-    Verbal::Numbers.to_words(1_002, 1).should == 'tysiąc dwie'
+    Verbal::Polish::Numbers.to_words(1, 0).should     == 'jeden'
+    Verbal::Polish::Numbers.to_words(1, 1).should     == 'jedna'
+    Verbal::Polish::Numbers.to_words(1, 2).should     == 'jedno'
+    Verbal::Polish::Numbers.to_words(2, 0).should     == 'dwa'
+    Verbal::Polish::Numbers.to_words(2, 1).should     == 'dwie'
+    Verbal::Polish::Numbers.to_words(2, 2).should     == 'dwa'
+    Verbal::Polish::Numbers.to_words(101, 0).should   == 'sto jeden'
+    Verbal::Polish::Numbers.to_words(101, 1).should   == 'sto jeden'
+    Verbal::Polish::Numbers.to_words(102, 0).should   == 'sto dwa'
+    Verbal::Polish::Numbers.to_words(102, 1).should   == 'sto dwie'
+    Verbal::Polish::Numbers.to_words(1_001, 0).should == 'tysiąc jeden'
+    Verbal::Polish::Numbers.to_words(1_001, 1).should == 'tysiąc jeden'
+    Verbal::Polish::Numbers.to_words(1_002, 0).should == 'tysiąc dwa'
+    Verbal::Polish::Numbers.to_words(1_002, 1).should == 'tysiąc dwie'
   end
 
 end
